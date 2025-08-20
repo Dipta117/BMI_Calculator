14e051eccbfe259e8968075886a4da763cb8c865
Dipta Chowdhury <141096160+Dipta117@users.noreply.github.com>
2025-08-20 12:52:56 +0600

    Fixing some issues and adding main function in student.java

--- Student.java
+++ Student.java
@@ -1,24 +1,24 @@
-public class Student
+public class student
 {
      private String name;
      private int roll;
-    
-}
 
-public Student(String name, int roll)
+
+
+public student(String name, int roll)
 {
     this.name=name;
     this.roll=roll;
-  
+
 }
 
-public string getname()
+public String getname()
 {
     return name;
 }
-public void setname()
+public void setname(String name)
 {
-    this.name= "Rahim";
+    this.name= name;
 }
 
 public int getroll()
@@ -26,7 +26,23 @@ public int getroll()
 return roll;
 }
 
-public void setroll()
+public void setroll(int roll)
 {
-    this.roll=38;
+    this.roll=roll;
+}
+
+public static void main(String[] args)
+{
+    student std=new student("Rahim",38);
+    System.out.println(std.getname());
+    System.out.println(std.getroll());
+
+    std.setname("karim");
+    std.setroll(49);
+
+      System.out.println(std.getname());
+    System.out.println(std.getroll());
+
+
+}
 }
