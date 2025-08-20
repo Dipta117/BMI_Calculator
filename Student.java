9855168f4bf77afc3c45b5d2fbdd1cc32cb3d0b0
Dipta Chowdhury <141096160+Dipta117@users.noreply.github.com>
2025-08-20 09:36:31 +0600

    Adding a Student.java file

---
+++ Student.java
@@ -0,0 +1,32 @@
+public class Student
+{
+     private String name;
+     private int roll;
+    
+}
+
+public Student(String name, int roll)
+{
+    this.name=name;
+    this.roll=roll;
+  
+}
+
+public string getname()
+{
+    return name;
+}
+public void setname()
+{
+    this.name= "Rahim";
+}
+
+public int getroll()
+{
+return roll;
+}
+
+public void setroll()
+{
+    this.roll=38;
+}
